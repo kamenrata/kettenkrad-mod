17c0e3d179102884923b574f41664cfcd15c32b7
kamenrata <maikurakosu@gmail.com>
2026-03-04 01:33:54 +0900

    Update KettenkradClientMod.java

--- src/main/java/com/example/kettenkrad/client/KettenkradClientMod.java
+++ src/main/java/com/example/kettenkrad/client/KettenkradClientMod.java
@@ -4,14 +4,22 @@ import com.example.kettenkrad.KettenkradMod;
 import com.example.kettenkrad.entity.ModEntities;
 import com.example.kettenkrad.inventory.KettenkradScreenHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen;
+import net.minecraft.client.gui.screen.ingame.HandledScreens;
 
 public class KettenkradClientMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // モデルレイヤーの登録（これが抜けていた！）
+        EntityModelLayerRegistry.registerModelLayer(
+            KettenkradModel.LAYER_LOCATION,
+            KettenkradModel::getTexturedModelData
+        );
+
         EntityRendererRegistry.register(ModEntities.KETTENKRAD, KettenkradRenderer::new);
+
         HandledScreens.register(KettenkradMod.KETTENKRAD_SCREEN_HANDLER, GenericContainerScreen::new);
     }
 }
