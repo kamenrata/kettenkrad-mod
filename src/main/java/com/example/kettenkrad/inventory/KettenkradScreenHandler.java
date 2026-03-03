52edca07b2d6edd2c2b026dacf0471d4e854da19
kamenrata <maikurakosu@gmail.com>
2026-03-04 00:41:27 +0900

    Update KettenkradScreenHandler.java

--- src/main/java/com/example/kettenkrad/inventory/KettenkradScreenHandler.java
+++ src/main/java/com/example/kettenkrad/inventory/KettenkradScreenHandler.java
@@ -6,51 +6,17 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.GenericContainerScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.screen.slot.Slot;
 
-public class KettenkradScreenHandler extends ScreenHandler {
-    private final Inventory inventory;
+public class KettenkradScreenHandler extends GenericContainerScreenHandler {
 
     public KettenkradScreenHandler(int syncId, PlayerInventory playerInv) {
         this(syncId, playerInv, new SimpleInventory(27));
     }
 
     public KettenkradScreenHandler(int syncId, PlayerInventory playerInv, Inventory inventory) {
-        super(KettenkradMod.KETTENKRAD_SCREEN_HANDLER, syncId);
-        checkSize(inventory, 27);
-        this.inventory = inventory;
-        inventory.onOpen(playerInv.player);
-        for (int row = 0; row < 3; row++)
-            for (int col = 0; col < 9; col++)
-                addSlot(new Slot(inventory, col + row*9, 8 + col*18, 18 + row*18));
-        for (int row = 0; row < 3; row++)
-            for (int col = 0; col < 9; col++)
-                addSlot(new Slot(playerInv, col + row*9 + 9, 8 + col*18, 84 + row*18));
-        for (int col = 0; col < 9; col++)
-            addSlot(new Slot(playerInv, col, 8 + col*18, 142));
-    }
-
-    @Override
-    public ItemStack quickMove(PlayerEntity player, int index) {
-        ItemStack result = ItemStack.EMPTY;
-        Slot slot = slots.get(index);
-        if (slot.hasStack()) {
-            ItemStack stack = slot.getStack();
-            result = stack.copy();
-            if (index < 27) {
-                if (!insertItem(stack, 27, slots.size(), true)) return ItemStack.EMPTY;
-            } else {
-                if (!insertItem(stack, 0, 27, false)) return ItemStack.EMPTY;
-            }
-            if (stack.isEmpty()) slot.setStack(ItemStack.EMPTY);
-            else slot.markDirty();
-        }
-        return result;
-    }
-
-    @Override
-    public boolean canUse(PlayerEntity player) {
-        return inventory.canPlayerUse(player);
+        super(KettenkradMod.KETTENKRAD_SCREEN_HANDLER, syncId, playerInv, inventory, 3);
     }
 }
